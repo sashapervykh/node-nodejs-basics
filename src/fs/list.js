5b89cf76708e63a6ce0b386dbe504e551cc698e5
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 17:32:49 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = fs.readdir(
+      path.resolve(import.meta.dirname, "files"),
+      { withFileTypes: true },
+      (error, files) => {
+        if (error) throw new Error("FS operation failed");
+        console.log(files.map((elem) => elem.name));
+      }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await list();
