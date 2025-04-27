b567fde402131e69cd9caec0b97105861db96111
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 17:26:44 +0300

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,27 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const files = fs.readdir(
+      path.resolve(import.meta.dirname, "files"),
+      { withFileTypes: true },
+      (error, files) => {
+        if (error) throw error;
+        if (!files.some((elem) => elem.name === "fileToRemove.txt")) {
+          throw new Error("FS operation failed");
+        }
+        fs.rm(
+          path.resolve(import.meta.dirname, "files", "fileToRemove.txt"),
+          (error) => {
+            if (error) throw new Error(error);
+          }
+        );
+      }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await remove();
