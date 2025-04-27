0a60b664c731f6829cbdd489914cfc3bfa4de5db
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 19:10:23 +0300

    refactor: change rename to async function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,26 +1,24 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 
 const rename = async () => {
   try {
-    const files = fs.readdir(
-      path.resolve(import.meta.dirname, "files"),
-      { withFileTypes: true },
-      (error, files) => {
-        if (error) throw error;
-        if (
-          !files.some((elem) => elem.name === "wrongFilename.txt") ||
-          files.some((elem) => elem.name === "properFilename.md")
-        ) {
-          throw new Error("FS operation failed");
-        }
-        fs.rename(
-          path.resolve(import.meta.dirname, "files", "wrongFilename.txt"),
-          path.resolve(import.meta.dirname, "files", "properFilename.md"),
-          (error) => {
-            if (error) throw err;
-          }
-        );
+    const files = await fs.readdir(path.resolve(import.meta.dirname, "files"), {
+      withFileTypes: true,
+    });
+
+    if (
+      !files.some((elem) => elem.name === "wrongFilename.txt") ||
+      files.some((elem) => elem.name === "properFilename.md")
+    ) {
+      throw new Error("FS operation failed");
+    }
+
+    fs.rename(
+      path.resolve(import.meta.dirname, "files", "wrongFilename.txt"),
+      path.resolve(import.meta.dirname, "files", "properFilename.md"),
+      (error) => {
+        if (error) throw err;
       }
     );
   } catch (error) {
