26ff69e7236ea8e387aefc7f0ea44864c9e7bb77
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 17:52:33 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const files = fs.readdir(
+      path.resolve(import.meta.dirname, "files"),
+      { withFileTypes: true },
+      (error, files) => {
+        if (error) throw error;
+        if (!files.some((elem) => elem.name === "fileToRead.txt")) {
+          throw new Error("FS operation failed");
+        }
+        fs.readFile(
+          path.resolve(import.meta.dirname, "files", "fileToRead.txt"),
+          "utf-8",
+          (error, data) => {
+            if (error) throw new Error(error);
+            console.log(data);
+          }
+        );
+      }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await read();
