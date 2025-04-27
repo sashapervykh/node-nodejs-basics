666eaa81b4686c3073d4c42abd21913fa9e7fdc5
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 19:07:09 +0300

    refactor: change read to async function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,28 +1,23 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 
 const read = async () => {
   try {
-    const files = fs.readdir(
-      path.resolve(import.meta.dirname, "files"),
-      { withFileTypes: true },
-      (error, files) => {
-        if (error) throw error;
-        if (!files.some((elem) => elem.name === "fileToRead.txt")) {
-          throw new Error("FS operation failed");
-        }
-        fs.readFile(
-          path.resolve(import.meta.dirname, "files", "fileToRead.txt"),
-          "utf-8",
-          (error, data) => {
-            if (error) throw new Error(error);
-            console.log(data);
-          }
-        );
-      }
+    const files = await fs.readdir(path.resolve(import.meta.dirname, "files"), {
+      withFileTypes: true,
+    });
+    if (!files.some((elem) => elem.name === "fileToRead.txt")) {
+      throw new Error("FS operation failed");
+    }
+
+    const data = await fs.readFile(
+      path.resolve(import.meta.dirname, "files", "fileToRead.txt"),
+      "utf-8"
     );
+
+    console.log(data);
   } catch (error) {
-    throw new Error(error);
+    throw new Error("FS operation failed");
   }
 };
 
