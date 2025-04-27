39738eb6314db1e3a14c1a8211ef31f7ede2c436
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 17:17:08 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const files = fs.readdir(
+      path.resolve(import.meta.dirname, "files"),
+      { withFileTypes: true },
+      (error, files) => {
+        if (error) throw error;
+        if (
+          !files.some((elem) => elem.name === "wrongFilename.txt") ||
+          files.some((elem) => elem.name === "properFilename.md")
+        ) {
+          throw new Error("FS operation failed");
+        }
+        fs.rename(
+          path.resolve(import.meta.dirname, "files", "wrongFilename.txt"),
+          path.resolve(import.meta.dirname, "files", "properFilename.md"),
+          (error) => {
+            if (error) throw err;
+          }
+        );
+      }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await rename();
