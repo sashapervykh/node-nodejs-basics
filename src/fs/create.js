f4d7f217d97b72a29e6ae21fbc7330991eb77566
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 17:06:44 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { error } from "node:console";
+import fs from "node:fs";
+import path from "node:path";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const files = fs.readdir(
+      path.resolve(import.meta.dirname, "files"),
+      { withFileTypes: true },
+      (error, files) => {
+        if (error) throw error;
+        if (files.some((elem) => elem.name === "fresh.txt")) {
+          throw new Error("FS operation failed");
+        }
+        fs.writeFile(
+          path.resolve(import.meta.dirname, "files", "fresh.txt"),
+          "I am fresh and young",
+          (error) => {
+            if (error) throw new Error(error);
+          }
+        );
+      }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await create();
