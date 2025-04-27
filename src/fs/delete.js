4afe6e3eaa1c70fe35d56b9a0e8f2f8f334f74f2
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-27 18:48:09 +0300

    refactor: change delete for async functions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,22 +1,24 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 
 const remove = async () => {
   try {
-    const files = fs.readdir(
+    const files = await fs.readdir(
       path.resolve(import.meta.dirname, "files"),
       { withFileTypes: true },
-      (error, files) => {
+      (error) => {
         if (error) throw error;
-        if (!files.some((elem) => elem.name === "fileToRemove.txt")) {
-          throw new Error("FS operation failed");
-        }
-        fs.rm(
-          path.resolve(import.meta.dirname, "files", "fileToRemove.txt"),
-          (error) => {
-            if (error) throw new Error(error);
-          }
-        );
+      }
+    );
+
+    if (!files.some((elem) => elem.name === "fileToRemove.txt")) {
+      throw new Error("FS operation failed");
+    }
+    fs.rm(
+      path.resolve(import.meta.dirname, "files", "fileToRemove.txt"),
+      {},
+      (error) => {
+        if (error) throw new Error(error);
       }
     );
   } catch (error) {
