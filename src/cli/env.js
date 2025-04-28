fe3201ed666f03fcebff1c91db754576e86db5b1
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-28 11:02:22 +0300

    feat: add parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const args = process.env;
+
+  const rssEnvVariables = Object.keys(args).filter((elem) =>
+    elem.startsWith("RSS_")
+  );
+  const result = [];
+  for (const env of rssEnvVariables) {
+    result.push([env, args[env]]);
+  }
+  console.log(result.map((elem) => elem.join("=")).join("; "));
 };
 
 parseEnv();
