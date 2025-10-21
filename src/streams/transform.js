cb8a80754b6b663a42e2ce27d4cd44399c3bfa1c
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-21 12:27:36 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-  // Write your code here
+  class ReverseTransform extends Transform {
+    constructor(options) {
+      super(options);
+    }
+
+    _transform(chunk, encoding, callback) {
+      try {
+        const resultString = chunk
+          .toString("utf-8")
+          .split("")
+          .reverse()
+          .join("");
+        callback(null, resultString);
+      } catch (err) {
+        callback(err);
+      }
+    }
+  }
+  const reverseTransform = new ReverseTransform();
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
