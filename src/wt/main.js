d73af1ab2ad8a9a74c3a55af6471f2dcef5989e6
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-21 12:34:23 +0300

    feat: add wt function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from "node:worker_threads";
+import path from "node:path";
+import os from "node:os";
+
 const performCalculations = async () => {
-  // Write your code here
+  const cpusAmount = os.availableParallelism();
+  const promises = Array.from({ length: cpusAmount })
+    .fill(null)
+    .map((_, index) => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(path.join(import.meta.dirname, "worker.js"), {
+          workerData: 10 + index,
+        });
+        worker.on("message", (message) => {
+          resolve({ status: "resolved", data: message });
+        });
+        worker.on("error", () => {
+          resolve({ status: "error", data: null });
+        });
+      });
+    });
+
+  const result = await Promise.all(promises);
+  console.log(result);
 };
 
 await performCalculations();
