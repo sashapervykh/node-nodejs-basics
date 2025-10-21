9a9dddc482f816810eee5a7aac09f283ecc81351
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-21 11:16:52 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const create = async () => {
-  // Write your code here
+  try {
+    await fs.writeFile(
+      path.join(import.meta.dirname, "files", "fresh.txt"),
+      "I am fresh and young",
+      { flag: "wx" }
+    );
+  } catch (err) {
+    if (err.code === "EEXIST") throw new Error("FS operation failed");
+    throw err;
+  }
 };
 
 await create();
