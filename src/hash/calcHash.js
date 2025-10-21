3d22a5c5f40ad0d507bd781039d96a91450bd8b9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-21 12:14:43 +0300

    feat: add calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createReadStream } from "node:fs";
+import { join } from "path";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-  // Write your code here
+  const hash = createHash("sha256");
+  const fileContent = createReadStream(
+    join(import.meta.dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  let result = "";
+
+  fileContent.pipe(hash);
+
+  hash.setEncoding("hex");
+
+  hash.on("data", (chunk) => {
+    result += chunk;
+  });
+
+  hash.on("end", () => {
+    console.log(result);
+    process.exit();
+  });
 };
 
 await calculateHash();
