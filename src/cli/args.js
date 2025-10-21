12cd84c57cec22ecdbd9c0c67f462996e6ee34e6
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-10-21 11:54:05 +0300

    feat: add args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = [];
+  for (let i = 2; i < process.argv.length; i += 2) {
+    const argName = process.argv[i].slice(2);
+    const argValue = process.argv[i + 1];
+    args.push(`${argName} is ${argValue}`);
+  }
+
+  console.log(args.join(", "));
 };
 
 parseArgs();
